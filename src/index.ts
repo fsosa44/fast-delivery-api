9690e17d7a27868ef3a98f5e9469e5722714addc
Juan Arismendi Diaz <141862378+McHeph@users.noreply.github.com>
2024-06-07 00:17:53 -0300

    fix cors url

--- src/index.ts
+++ src/index.ts
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://fastdelivery-clone.vercel.app",
+    origin: "https://fastdelivery.vercel.app",
     credentials: true,
   })
 );
