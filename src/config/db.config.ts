68f11cfd3b590a29cfe239452d48aee345a88f60
Juan Arismendi <juanarismendidiaz@gmail.com>
2024-06-06 22:51:40 -0300

    fix connection with db

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,3 +1,5 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize(process.env.DB_CONNECTION as string);
+const db = new Sequelize(process.env.DB_CONNECT as string, {
+  dialect: "postgres",
+});
 export default db;
