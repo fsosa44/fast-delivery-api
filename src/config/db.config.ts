89e00719100a94a57fe1c4a6137ff0600e8cf627
Juan Arismendi <juanarismendidiaz@gmail.com>
2024-06-06 22:56:10 -0300

    Fix typo

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize(process.env.DB_CONNECT as string, {
+const db = new Sequelize(process.env.DB_CONNECTION as string, {
   dialect: "postgres",
 });
 export default db;
